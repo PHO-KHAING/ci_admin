3300f10a45a7255d563723fe036f06054ed68ca7
Pho Khaing <khaing.pho1991@gmail.com>
2021-09-05 13:55:35 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'db',
+	'hostname' => '3.0.16.134',
 	// 'hostname'=>'localhost'
 	'username' => 'root',
 	'password' => 'root',
